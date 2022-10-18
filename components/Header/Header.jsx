900063c9a50977513e9035b0df9bd8c94bcf1adb
Raphael Coelho de Lima <rcl.lp@hotmail.com>
2022-10-18 14:06:04 -0400

    headeratualizado

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -79,7 +79,7 @@ const Header = () => {
               <div className={`${classes.nav__right}`}>
                 <p className=" d-flex align-items-center gap-2 mb-0">
                   {" "}
-                  <i className="ri-phone-line"></i> +14374223542{" "}
+                  <i className="ri-phone-line"></i> <a>+14374223542</a>{" "}
                 </p>
               </div>
             </div>
